deeadae3f133415e1c05b47441ecbccf5025df0e
keieng <105902831+keieng@users.noreply.github.com>
2022-11-20 00:08:55 +0900

    検索機能実装

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,8 @@
 class Movie < ApplicationRecord
   validates :name, uniqueness: { message: "映画名が既に登録済みです。" }
   validates :name, presence: { message: "映画名を入力してください。" }
+
+  scope :word_search, ->(keyword) { where("name LIKE ?", "%#{keyword}%").or(where("description LIKE ?", "%#{keyword}%")) }
+  scope :showing_search, ->(is_showing) { where(is_showing: is_showing) }
+  scope :search, ->(name, is_showing) { name_search(name).showing_search(is_showing) }
 end
