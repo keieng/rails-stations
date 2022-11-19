5dbf14bb958cf4a79a55e3a2168abfadf6a4c882
keieng <105902831+keieng@users.noreply.github.com>
2022-11-19 19:37:12 +0900

    新規追加機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get "movies", to: "movies#index"
-    get "movies/index"
-    get "movies/search"
+    resources :movies
+    # get "movies/search"
   end
   resources :movies
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
